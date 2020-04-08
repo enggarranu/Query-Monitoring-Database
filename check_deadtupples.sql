c2288132fc1a5785c440763743655b5a687e4698
Enggar Ranu Hariawan <enggar.ranu@gmail.com>
2020-04-08 13:08:52 +0700

    Create check_deadtupples.sql

---
+++ check_deadtupples.sql
@@ -0,0 +1,1 @@
+SELECT schemaname, relname, n_live_tup ,n_dead_tup, ((case when n_dead_tup = 0 then 1 else n_dead_tup end)::float  / (case when n_live_tup = 0 then 1 else n_live_tup end)::float)*100 as "dead_tupple (percentage)" FROM pg_stat_user_tables where schemaname = 'public' and n_live_tup > 0 and n_dead_tup > 0 order by 5 desc;
