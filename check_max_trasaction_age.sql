f4801f937fabdd43fb93925f8a2fa10a5dcb7420
Enggar Ranu Hariawan <enggar.ranu@gmail.com>
2020-04-08 13:08:04 +0700

    Create check_max_trasaction_age.sql

---
+++ check_max_trasaction_age.sql
@@ -0,0 +1,5 @@
+SELECT c.oid::regclass as table_name,
+       greatest(age(c.relfrozenxid),age(t.relfrozenxid)) as age
+FROM pg_class c
+LEFT JOIN pg_class t ON c.reltoastrelid = t.oid
+WHERE c.relkind IN ('r', 'm') order by 2 desc;
